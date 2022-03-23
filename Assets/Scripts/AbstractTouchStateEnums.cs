c34129cf40caccb6f20b6ad1948c78da6f427fd0
MuhammetInce <muhammetinceee@gmail.com>
2022-03-24 01:17:46 +0300

    + Add : TouchState Enum but It Was Not Used

---
+++ Assets/Scripts/AbstractTouchStateEnums.cs
@@ -0,0 +1,16 @@
+﻿
+    public abstract class AbstractTouchStateEnums
+    {
+        public enum TouchState
+        {
+            OnPanel,
+            OnDrag,
+        }
+
+        public static TouchState CurrentState = TouchState.OnPanel;
+
+        public static void SetTouchEnum(TouchState setState)
+        {
+            CurrentState = setState;
+        }
+    }
