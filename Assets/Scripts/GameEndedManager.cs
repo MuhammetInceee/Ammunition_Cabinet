1b2542a7e387b4454837e6d9e0d8e6df4bb7139b
MuhammetInce <muhammetinceee@gmail.com>
2022-04-11 13:08:36 +0300

    + Add : Door Open Anim with DoTween

--- Assets/Scripts/GameEndedManager.cs
+++ Assets/Scripts/GameEndedManager.cs
@@ -3,24 +3,31 @@ using System.Collections.Generic;
 using DG.Tweening;
 using MuhammetInce.DesignPattern.Singleton;
 using UnityEngine;
-using UnityEngine.Serialization;
 
 public class GameEndedManager : Singleton<GameEndedManager>
 {
     [Header("Doors"), Space]
     [SerializeField] private GameObject rightDoor;
     [SerializeField] private GameObject leftDoor;
-
+    
     [Header("DoorAngles"), Space] 
-    [SerializeField] private Vector3 rightDoorAngle;
-    [SerializeField] private Vector3 leftDoorAngle;
+    [SerializeField] private Vector3 rightDoorCloseAngle;
+    [SerializeField] private Vector3 leftDoorCloseAngle;
+    [Space]
+    [SerializeField] private Vector3 rightDoorOpenAngle;
+    [SerializeField] private Vector3 leftDoorOpenAngle;
 
     [Header("Durations"), Space] 
-    [SerializeField] private float doorCloseDuration;
+    [SerializeField] private float doorAnimDuration;
 
+    public void DoorOpener()
+    {
+        rightDoor.transform.DORotate(rightDoorOpenAngle, doorAnimDuration);
+        leftDoor.transform.DORotate(leftDoorOpenAngle, doorAnimDuration);
+    }
     public void DoorCloser()
     {
-        rightDoor.transform.DORotate(rightDoorAngle, doorCloseDuration);
-        leftDoor.transform.DORotate(leftDoorAngle, doorCloseDuration);
+        rightDoor.transform.DORotate(rightDoorCloseAngle, doorAnimDuration);
+        leftDoor.transform.DORotate(leftDoorCloseAngle, doorAnimDuration);
     }
 }
